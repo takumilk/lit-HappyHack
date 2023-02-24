ce4def635e7862e4488a470379b639758e4b2f26
github <takuseifu@gmail.com>
2023-02-24 11:30:06 +0900

    first-commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function($) {
+    $('.bg-slider').bgSwitcher({
+        images: ['images/background1.jpg','images/background2.jpg','images/background3.jpg'],
+        interval: 5000,
+        loop: true
+    });
+
+
+});
---
+++ script.js
@@ -0,0 +1,11 @@
+
+jQuery(function ($) {		
+    $('.bg-slider').bgSwitcher({
+        images: ['images/background1.jpg', 'images/background2.jpg', 'images/background3.jpg'], // 切り替える背景画像を指定
+        interval: 6000, // 背景画像を切り替える間隔を指定 3000=3秒
+        loop: true, // 切り替えを繰り返すか指定 true=繰り返す　false=繰り返さない
+        effect: "fade", // エフェクトの種類をfade,blind,clip,slide,drop,hideから指定
+        duration: 600, // エフェクトの時間を指定します。
+        easing: "swing" // エフェクトのイージングをlinear,swingから指定
+    });			
+});
