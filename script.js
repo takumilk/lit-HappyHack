63884b5040d9ef5fe9b50a90af2208c9193fc78c
github <takuseifu@gmail.com>
2023-02-25 15:25:10 +0900

    third-commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 jQuery(function ($) {		
-    $('.bg-slider').bgSwitcher({
+    $('.bg-switcher').bgSwitcher({
         images: ['images/background1.jpg', 'images/background2.jpg', 'images/background3.jpg'], // 切り替える背景画像を指定
         interval: 6000, // 背景画像を切り替える間隔を指定 3000=3秒
         loop: true, // 切り替えを繰り返すか指定 true=繰り返す　false=繰り返さない
